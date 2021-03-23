39862686bb10340a0e1bff03e2aa620923cd998a
aghaz <aghaz@LAPTOP-L2PTAS43.fios-router.home>
2021-03-22 21:00:16 -0400

    Added Macaroni Cheese line

--- src/github/GithubPractice.java
+++ src/github/GithubPractice.java
@@ -5,8 +5,11 @@ public class GithubPractice {
 	public static void main(String[] args) {
 		
 		System.out.println("Im Hungry");
+		
+		System.out.println("Macaroni And Cheese");
 	}
 
 	public static void printString(String str){
 		System.out.println(str);
+	}
 }
